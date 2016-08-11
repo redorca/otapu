55223f8e69d33063effae902cb58024a13397c75
redorca <uberfoot@yahoo.com>
2016-08-11 14:54:14 -0700

    Let each subclass run __init__ after parent's
        Let each subclass run __init__() to set the identifying error code
    and message.  Each sub __init__() call needs to explictily run
    TftpException.__init__(self)

--- tftp/exceptions.py
+++ tftp/exceptions.py
@@ -14,52 +14,76 @@ class TftpException(Exception):
         Exception.__init__(self)
 
     def __str__(self):
-        pass
+        return "Error %: %", self.code, self.msg
 
 class Undefined(TftpException):
     '''
         Error code 0:  no such error.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class FileNotFound(TftpException):
     '''
         File referenced by request action is not found.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class AccessViolation(TftpException):
     '''
         Insufficient privileges to access resource.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class IllegalOp(TftpException):
     '''
         Illegal Tftp opcode.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class OutOfSpace(TftpException):
     '''
         No more space for transfer.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class UnknownTID(TftpException):
     '''
         Transfer ID from target is unrecognized.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class FileExists(TftpException):
     '''
         File to write already exists on the server or on this host.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
 
 class NoSuchUser(TftpException):
     '''
         No uid found for transfer.
     '''
-    pass
+    def __init__(self, pkt):
+        TftpException.__init__(self, pkt)
+        self.code = 0
+        self.msg = "No such error"
